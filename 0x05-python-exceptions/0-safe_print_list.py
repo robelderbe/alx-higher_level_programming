eec2de95d636e5d6d4c9f3f1acef99c0e325b75b
robelderbe <robelderbe@gmail.com>
2022-09-12 12:12:43 -0700

    adding function prints any type of variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-<<<<<<< HEAD
     count = 0
-=======
-     count = 0
->>>>>>> 2f479ab9cc953e599ab4e5772bb870bc594187c1
 
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end='')
-            except:
-                break
-            else:
-                count += 1
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
+            count += 1
 
-        print()
-        return (count)
+    print()
+    return (count)
