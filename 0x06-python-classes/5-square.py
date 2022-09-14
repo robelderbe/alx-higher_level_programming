cc51b9a9e788f0feabf158fedd01ee7ca8d1ad14
robelderbe <robelderbe@gmail.com>
2022-09-14 11:58:56 -0700

    adding a class Square that defines a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,14 +4,16 @@
 
 class Square:
     """Represents a square
+
     Attributes:
         __size (int): size of a side of the square
     """
-
     def __init__(self, size=0):
         """initializes the square
+
         Args:
             size (int): size of a side of the square
+
         Returns:
             None
         """
@@ -19,6 +21,7 @@ class Square:
 
     def area(self):
         """calculates the square's area
+
         Returns:
             The area of the square
         """
@@ -27,6 +30,7 @@ class Square:
     @property
     def size(self):
         """getter of __size
+
         Returns:
             The size of the square
         """
@@ -35,8 +39,10 @@ class Square:
     @size.setter
     def size(self, value):
         """setter of __size
+
         Args:
             value (int): size of a side of the square
+
         Returns:
             None
         """
